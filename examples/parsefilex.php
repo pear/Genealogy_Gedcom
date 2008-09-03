569eba72db3d4249d53b7ecfd4436e85e6d58bc2
Ken Guest <kguest@php.net>
2008-09-03 21:37:48 +0000

    parse any .ged file specified on the command line
    git-svn-id: http://svn.php.net/repository/pear/packages/Genealogy_Gedcom/trunk@265864 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/parsefilex.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL);
+
+// for windows users
+//ini_set('include_path', '.;'.$_SERVER['DOCUMENT_ROOT'].'/pear');
+
+require_once 'Benchmark/Timer.php';
+require_once 'Genealogy/Gedcom.php';
+
+//$timer =& new Benchmark_Timer();
+//$timer->setMarker('Start');
+$file = $_SERVER['argv'][1];
+$ged =& new Genealogy_Gedcom($file);
+//$timer->stop();
+//$timer->display();
+
+echo 'getNumberOfIndividuals :'.$ged->getNumberOfIndividuals();
+echo '<br>';
+echo 'getNumberOfFamilies :'.$ged->getNumberOfFamilies();
+echo '<br>';
+echo 'getNumberOfObjects :'.$ged->getNumberOfObjects();
+echo '<br>';
+echo 'Last Update :'.$ged->getLastUpdate();
+echo '<br>';
+
+echo '<pre>';
+print_r($ged->GedcomIndividualsTreeObjects);
+print_r($ged->GedcomFamiliesTreeObjects);
+print_r($ged->GedcomObjectsTreeObjects);
+print_r($ged->GedcomHeaderTreeObject);
+print_r($ged->getIndividual('I1'));
+print_r($ged->getFamily('F1'));
+print_r($ged->getObject('O1'));
+echo '</pre>';
+?>
